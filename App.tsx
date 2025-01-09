b06e5640528702386a996fed24e82e10bc31b3d4
Matheus Alves <matheusalves@unifor.br>
2025-01-09 15:06:13 -0300

    loading

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import { StyleSheet, Text, View, StatusBar, StatusBarStyle } from 'react-native'
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 
-import Login from './src/pages/Auth/Login';
 import Routes from './src/routes/routes';
 
 export default function App() {
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,13 +1,29 @@
+import { View, ActivityIndicator } from "react-native";
+
 import AppRoutes from "./app.routes";
 import AuthRoutes from "./auth.routes";
 
 export default function Routes() {
-    const isAuthenticated = false;
-    const loading = false;
+  const isAuthenticated = false;
+  const loading = true;
 
+  if (loading) {
     return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "#1D1D2E",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <ActivityIndicator size={60} color="#FFF"/>
+      </View>
+    );
+  }
 
-        // If isAuthenticated equals true => AppRoutes else AuthRoutes
-        isAuthenticated ? <AppRoutes/> : <AuthRoutes/>
-    )
+  return (
+    // If isAuthenticated equals true => AppRoutes else AuthRoutes
+    isAuthenticated ? <AppRoutes /> : <AuthRoutes />
+  );
 }
